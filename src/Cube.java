521f26da7684dc4127726b36f9ec7f2fff30b71f
Michael Wu <michaeldubyu@gmail.com>
2013-03-27 20:58:24 -0700

    with texture loading and a cube rendering and basic lighting

--- src/Cube.java
+++ src/Cube.java
@@ -1,22 +1,21 @@
 
 public class Cube {
 //represent a singular cube, one of 27 in this game of rubicks cube
-	private Color  color0; //x
-	private Color  color1; //y
-	private Color  color2; //z
-	private int[][] position = new int[3][3]; //where the cube is initially placed
+	private Color color0;  //front
+	private Color color1;  //right
+	private Color color2;  //left
+	private Color color3;  //top
+	private Color color4;  //bottom
+	private Color color5;  //back
 	
-	public Cube(){
-		this.color0 = null;
-		this.color1 = null;
-		this.color2 = null;
-		position = null;
-	}
-	
-	public Cube (Color color0 ,Color color1 ,Color color2){
+	public Cube (Color color0 ,Color color1 ,Color color2, Color color3,
+				 Color color4, Color color5){
 		this.color0 = color0;
-		this.color1=  color1;
+		this.color1 = color1;
 		this.color2 = color2;
+		this.color3 = color3;
+		this.color4 = color4;
+		this.color5 = color5;
 	}
 
 	public Color getColor0() {
@@ -42,4 +41,29 @@ public class Cube {
 	public void setColor2(Color color2) {
 		this.color2 = color2;
 	}
+
+	public Color getColor3() {
+		return color3;
+	}
+
+	public void setColor3(Color color3) {
+		this.color3 = color3;
+	}
+
+	public Color getColor4() {
+		return color4;
+	}
+
+	public void setColor4(Color color4) {
+		this.color4 = color4;
+	}
+
+	public Color getColor5() {
+		return color5;
+	}
+
+	public void setColor5(Color color5) {
+		this.color5 = color5;
+	}
+
 }
--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,26 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+
+import de.matthiasmann.twl.utils.PNGDecoder;
 import static org.lwjgl.util.glu.GLU.*;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Game{
 	private static CubeFace[] faces;
 	private static Cube[] cubes;
+	
+	private static int tex;
 	int fps;
 	long lastFPS;
 	
@@ -18,11 +29,16 @@ public class Game{
 	public static Color RED = new Color(1.0,0.0,0.0);
 	public static Color GREEN = new Color(0.0,1.0,0.0);
 	public static Color BLUE = new Color(0.0,0.0,1.0);
-	public static Color HALF = new Color(0.5,0.5,0.5);
-	public static Color QUARTER = new Color(0.75,0.75,0.75);
+	public static Color HALF = new Color(0.5,1.0,1.0);
+	public static Color QUARTER = new Color(1.0,0.75,1.0);
+	
+	static FloatBuffer matDiffuse = BufferUtils.createFloatBuffer(4);
+	static FloatBuffer matAmbient = BufferUtils.createFloatBuffer(4);
+    static FloatBuffer matSpecular = BufferUtils.createFloatBuffer(4);
 	
 	static float scale = 0.5f;
-	final static float distZ = 25;
+	final static float distZ = 20;
+	final static float distY = 30;
 	static float angleX = 0;
 	static float angleY = 0;
 
@@ -31,87 +47,162 @@ public class Game{
 		faces = new CubeFace[7];
 		cubes = new Cube[27];
 		
-		for (int l=0;l<9;l++){
-			//initialize the front 9 cubes
-			Cube c = new Cube();
-
-			c.setColor0(BLUE);
-			c.setColor1(WHITE);
-			c.setColor2(RED);
-			
-			if (l==1 || l==4 || l==7){
-				c.setColor1(BLACK); 
-				c.setColor2(BLACK);
-			}
-			
-			if (l==2 || l==5 || l==8){
-				c.setColor2(BLACK);
-			}
-			
-			if (l%3 == 0){
-				c.setColor1(BLACK);
-			}
-	
+		Cube c = new Cube(WHITE, QUARTER, RED, GREEN, BLUE, HALF);
+		for (int l=0;l<27;l++){
 			cubes[l] = c;
-		}	
+		}			
+	    tex = setupTextures("res/textures/table.png");
+	    
+		matSpecular.put(1.0f).put(1.0f).put(1.0f).put(1.0f).flip();
+		matAmbient.put(0.5f).put(0.5f).put(0.5f).put(1.0f).flip();
+		matDiffuse.put(0.2f).put(0.2f).put(0.2f).put(1.0f).flip();
 		
-		for (int l=9;l<18;l++){
-			//initialize the middle 9 cubes
-			Cube c = new Cube();
+	    FloatBuffer lightPosition = BufferUtils.createFloatBuffer(4);
+	    lightPosition.put(0.0f).put(40.0f).put(0.0f).put(0.0f).flip();
+	    
+	    FloatBuffer spotDirection = BufferUtils.createFloatBuffer(4);
+	    lightPosition.put(0.0f).put(0.0f).put(0.0f).put(0.0f).flip();
+	    
+	    FloatBuffer ambient = BufferUtils.createFloatBuffer(4);
+		ambient.put(0.6f).put(0.5f).put(0.5f).put(1.0f).flip();
 
-			c.setColor0(GREEN);
-			c.setColor1(HALF);
-			c.setColor2(QUARTER);
-			
-			cubes[l] = c;		
-		}
-		
-		for (int l=18;l<27;l++){
-			//initialize the back 9 cubes
-			Cube c = new Cube();
+	    glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+	    
+	    glEnable(GL_LIGHTING);
+	    glEnable(GL_LIGHT0);
+	    glEnable(GL_DEPTH_TEST);
+	    
+	    glShadeModel (GL_SMOOTH);
+	    glEnable(GL_COLOR_MATERIAL);
+	    
+	    glLight(GL_LIGHT0, GL_POSITION, lightPosition);
+	    glLight(GL_LIGHT0, GL_SPOT_DIRECTION, spotDirection);
 
-			c.setColor0(WHITE);
-			c.setColor1(WHITE);
-			c.setColor2(WHITE);
-			
-			cubes[l] = c;		
-		}
+	    glLightModel(GL_LIGHT_MODEL_AMBIENT, ambient);
+	    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+	    
+	}
+	
+	private static int setupTextures(String filename) {
+	    IntBuffer tmp = BufferUtils.createIntBuffer(1);
+	    glGenTextures(tmp);
+	    tmp.rewind();
+	    try {
+	        InputStream in = new FileInputStream(filename);
+	        PNGDecoder decoder = new PNGDecoder(in);
+
+	        ByteBuffer buf = ByteBuffer.allocateDirect(4 * decoder.getWidth() * decoder.getHeight());
+	        decoder.decode(buf, decoder.getWidth() * 4, PNGDecoder.Format.RGBA);
+	        buf.flip();
+
+	        glBindTexture(GL_TEXTURE_2D, tmp.get(0));
+	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+	                GL_NEAREST);
+	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+	                GL_NEAREST);
+	        glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);
+	        int unsigned = (buf.get(0) & 0xff);
+
+	    } catch (java.io.FileNotFoundException ex) {
+	        System.out.println("Error " + filename + " not found");
+	    } catch (java.io.IOException e) {
+	        System.out.println("Error decoding " + filename);
+	    }
+	    tmp.rewind();
+	    return tmp.get(0);
+	}
+	
+	public static void drawTable() {
+        glEnable(GL_TEXTURE_2D);
+	    glBindTexture(GL_TEXTURE_2D, tex);
+	    glMaterial(GL_FRONT, GL_SPECULAR, matSpecular);
+	    glMaterialf(GL_FRONT, GL_SHININESS, 50.0f);
+	    glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, matAmbient);
+	    glBegin(GL_QUADS);
+	    	glTexCoord2d(0,0);
+	    	glNormal3f(0,1.0f,0);
+	    	glVertex3f(-100,0,-100);
+	    	
+	    	glTexCoord2d(0,1);
+	    	glNormal3f(0,1.0f,0);
+	    	glVertex3f(100,0,-100);
+	    	
+	    	glTexCoord2d(1,1);
+	    	glNormal3f(0,1.0f,0);
+	    	glVertex3f(100,0,100);
+	    	
+	    	glTexCoord2d(1,0);	 
+	    	glNormal3f(0,1.0f,0);
+	    	glVertex3f(-100,0,100);
+	    glEnd();
+	    glDisable(GL_TEXTURE_2D);
 	}
 	
     public static void drawCube() {
-    	//designate cube[0...9] to be the first 9 cubes
-    	//designate cube[9...18] to be the middle 9 cubes
-    	//designate cube[18..27] to be the final 9 cubes
+
     	//iterate over list of cubes, and draw
     	float spacing = 2.5f * scale;
-    	for (int i = 0, cc = 0;i < 3 && cc < 9;i++){
-    		glPushMatrix();
-    		for (int j = 0;j < 3; j++, cc++){
-	    		//rendering the front 9 cubes
-	    		Cube c = cubes[cc];
-	    		
-	    		Color z = c.getColor0();
-	    		Color x = c.getColor1();
-	    		Color y = c.getColor2();
-	    		
-	    		glTranslatef(spacing,0,0);
-	    		drawQuad(z, scale);
-	    		
-	    		glPushMatrix();
-	    		glRotatef(90,0,1,0);
-	    		drawQuad(x, scale);
-	    		glPopMatrix();
-	    		
-	    		glPushMatrix();
-	    		glRotatef(-90,0,1,0);
-	    		drawQuad(y, scale);
-	    		glPopMatrix();
-    		}
-    		glPopMatrix();
-    		glTranslatef(0,spacing,0);
-    	}
+    	int cc = 0;
     	
-
+    	glTranslatef(-2.5f,0,0);
+    	
+    	for (int k=0;k<3 && cc < 27;k++){
+    		glPushMatrix();
+        	for (int i = 0;i < 3 && cc < 27;i++){
+        		glPushMatrix();
+        		for (int j = 0;j < 3 && cc < 27; j++, cc++){
+    	    		//rendering the front 9 cubes
+    	    		Cube c = cubes[cc];
+    	    		
+    	    		Color front = c.getColor0(); 
+    	    		Color right = c.getColor1(); 
+    	    		Color left = c.getColor2(); 
+    	    		Color top = c.getColor3();
+    	    		Color bottom = c.getColor4();
+    	    		Color back = c.getColor5();
+    	    		
+    	    		glTranslatef(spacing,0,0);
+    	    		glNormal3f(0f,0f,1f);
+    	    		drawQuad(front, scale);
+    	    		
+    	    		glPushMatrix();
+    	    		glRotatef(90,0,1,0);
+    	    		glNormal3f(1.0f,0,0);
+    	    		drawQuad(right, scale);
+    	    		glPopMatrix();
+    	    		
+    	    		glPushMatrix();
+    	    		glRotatef(-90,0,1,0);
+    	    		glNormal3f(-1.0f,0,0);
+    	    		drawQuad(left, scale);
+    	    		glPopMatrix();
+    	    		
+    	    		glPushMatrix();
+    	    		glRotatef(90,1,0,0);
+    	    		glNormal3f(0,1.0f,0);
+    	    		drawQuad(top, scale);
+    	    		glPopMatrix();
+    	    		
+    	    		glPushMatrix();
+    	    		glRotatef(-90,1,0,0);
+    	    		glNormal3f(0,-1.0f,0);
+    	    		drawQuad(bottom,scale);
+    	    		glPopMatrix();
+    	    		
+    	    		glPushMatrix();
+    	    		glRotatef(180,0,1,0);
+    	    		glNormal3f(0,0,-1.0f);
+    	    		drawQuad(back,scale);
+    	    		glPopMatrix();
+    	    		
+        		}
+        		glPopMatrix();
+        		glTranslatef(0,spacing,0);
+        	}
+        	glPopMatrix();
+        	glTranslatef(0,0,-spacing);
+    	}
     }
     
     public static void drawQuad(Color c, float scale){
@@ -141,11 +232,11 @@ public class Game{
 
         // init OpenGL here
         initCube();
-        glShadeModel(GL_SMOOTH);              // Enable Smooth Shading
-        glClearColor(0.0f, 0.0f, 0.0f, 0.5f);    // Black Background
-        glClearDepth(1.0f);                      // Depth Buffer Setup
-        glEnable(GL_DEPTH_TEST);              // Enables Depth Testing
-        glDepthFunc(GL_LEQUAL);               // The Type Of Depth Testing To Do
+        glShadeModel(GL_SMOOTH);              
+        glClearColor(0.0f, 0.0f, 0.0f, 0.5f);    
+        glClearDepth(1.0f);                      
+        glEnable(GL_DEPTH_TEST);              
+        glDepthFunc(GL_LEQUAL);               
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  
  
         lastFPS = getTime(); //init FPS time
@@ -156,21 +247,27 @@ public class Game{
 		    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 			
 		    // set the color of the quad (R,G,B,A)
-		    glColor3f(0.5f,0.5f,1.0f);
 		    glMatrixMode(GL_PROJECTION);
 	        
 		    // set up view here
 		    glLoadIdentity();
-		    gluPerspective(45, 1, 1, 100 ); //near of 1, far of 100
-	        
-		    glTranslatef(0,0,-distZ);
-		    glRotatef(angleX,1,0,0);
-		    glRotatef(angleY,0,1,0);
-	       
+		    gluPerspective(45, 1, 1, 250 ); //near of 1, far of 100
+	      
 	        glMatrixMode(GL_MODELVIEW);
+	        glLoadIdentity();	
+
+	        glTranslatef(0,-distY,-distZ);
+			glRotatef(angleX,1,0,0);
+			glRotatef(angleY,0,1,0);
+		    
+			//draw table
+	        glPushMatrix();
+	        drawTable();
+	        glPopMatrix();
 	        
 	        //draw updates here
 	        glPushMatrix();
+	        glTranslatef(0,distY,0);
 			drawCube();
 			glPopMatrix();
 
@@ -186,16 +283,16 @@ public class Game{
 
     public void pollInput() {
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-        	angleY += 10;
+        	angleY += 5;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-        	angleY -= 10;
+        	angleY -= 5;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
-        	angleX += 10;
+        	angleX += 5;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
-        	angleX -= 10;
+        	angleX -= 5;
         }
     }
     
